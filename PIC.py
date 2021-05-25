a93332ff7bf4d99983dd91160bcb06e03ab5c3c0
Russell Hooper <rhoope@sandia.gov>
2021-05-25 15:14:58 -0600

    add gradient function and test
     - also rename "phi" --> "V" for potential field

--- PIC.py
+++ PIC.py
@@ -12,19 +12,40 @@ def solvePotentialGS(dx, rho, max_it) :
 
     dx2 = dx*dx       # precompute dx*dx
     SOR_W = 1.4;
-    phi = 0.0*rho
-    ni = phi.shape[0] # number of mesh nodes
+    V = 0.0*rho
+    ni = V.shape[0] # number of mesh nodes
     #print("The size of the numpy array is: "+str(ni))
 
     # solve potential
     for solver_it in range(max_it) :
-        phi[0] = 0.0      # dirichlet boundary on left,  V = 0
-        phi[ni-1] = 0.0   # dirichlet boundary on right, V = 0
+        V[0] = 0.0      # dirichlet boundary on left,  V = 0
+        V[ni-1] = 0.0   # dirichlet boundary on right, V = 0
 		
-        # Gauss Seidel method, phi[i-1]-2*phi[i]+phi[i+1] = -dx^2*rho[i]/eps_0*/
+        # Gauss Seidel method, V[i-1]-2*V[i]+V[i+1] = -dx^2*rho[i]/eps_0*/
         for i in range(1,ni-1) :
-            g = 0.5*(phi[i-1] + phi[i+1] + dx2*rho[i]/EPS_0)
-            phi[i] = phi[i] + SOR_W*(g-phi[i]) # SOR
+            g = 0.5*(V[i-1] + V[i+1] + dx2*rho[i]/EPS_0)
+            V[i] = V[i] + SOR_W*(g-V[i]) # SOR
 
-    return phi;	
+    return V;	
+
+
+# computes electric field by differentiating potential
+def computeGrad(dx, V, second_order) :
+
+    ni = V.shape[0] # number of mesh nodes
+    grad = 0.0*V
+
+    # central difference on internal nodes
+    for i in range(1,ni-1) :
+        grad[i] = (V[i+1]-V[i-1])/(2*dx);
+
+    # boundaries
+    if second_order :
+        grad[0] = -(3*V[0]-4*V[1]+V[2])/(2*dx)
+        grad[ni-1] = -(-V[ni-3]+4*V[ni-2]-3*V[ni-1])/(2*dx)
+    else : # first order
+        grad[0] = -(V[0]-V[1])/dx
+        grad[ni-1] = -(V[ni-2]-V[ni-1])/dx
+    
+    return grad
 
--- unit_tester.py
+++ unit_tester.py
@@ -7,7 +7,7 @@ import PIC # Our evolving ES PIC library we want to test
 
 class TestSolver(unittest.TestCase):
 
-    def test1(self):
+    def _test_V_solve(self):
 
         dx = 0.01
         N = int(1/dx)
@@ -16,17 +16,17 @@ class TestSolver(unittest.TestCase):
 
         # Call the numerical solver
         max_iters = 1000
-        phi = PIC.solvePotentialGS(dx, rho, max_iters)
+        V = PIC.solvePotentialGS(dx, rho, max_iters)
 
         # Compare to the known exact solution
         exact = lambda x : 0.5*x*(1-x)
         phi_e = exact(x)
-        e_norm = np.linalg.norm((phi-phi_e))
-        print("Error norm: "+str(e_norm))
+        #error_norm = np.linalg.norm((V-phi_e))
+        #print("Error norm: "+str(error_norm))
 
         # Plot the two solutions
         fig, ax = plt.subplots()
-        ax.plot(x, phi, marker='x', label='FD')
+        ax.plot(x, V, marker='x', label='FD')
         ax.plot(x, phi_e, label='exact')
         ax.legend()
 
@@ -34,12 +34,47 @@ class TestSolver(unittest.TestCase):
         #ax.grid()
 
         fig.savefig("test.png")
-        plt.show()
+        #plt.show()
 
         # Test that the solver produced a solution that agrees with the
         # exact solution to within (default) tolerance
 
-        self.assertTrue(np.allclose(phi, phi_e))
+        self.assertTrue(np.allclose(V, phi_e))
+
+
+    def test_dVdx(self):
+
+        dx = 0.01
+        N = int(1/dx)
+        x = np.linspace(0.0, 1.0, N+1)
+
+        # Create an exact V field (polynomial) that we will numerically differentiate
+        exactV = lambda x : 0.5*x*(1-x)
+        V = exactV(x)
+
+        # And create the field of exact gradient values
+        exactG = lambda x : 0.5 - x
+        exactGrad = exactG(x)
+
+        # Call the numerical gradient using first-order approximation for domain boundaries
+        use_second_order = False
+        gradV = PIC.computeGrad(dx, V, second_order=False)
+
+        # Compute the error and check that it is within tolerance but also outside a tighter tolerance
+        error_norm = np.linalg.norm((gradV-exactGrad))
+        #print("Error norm: "+str(error_norm))
+        self.assertTrue(error_norm < 1.e-2)
+        self.assertFalse(error_norm < 1.e-3)
+
+
+        # Now call the numerical gradient using second-order approximation for domain boundaries
+        use_second_order = True
+        gradV = PIC.computeGrad(dx, V, second_order=True)
+
+        # Compute the error and check that it is very close to machine precision
+        error_norm = np.linalg.norm((gradV-exactGrad))
+        #print("Error norm: "+str(error_norm))
+        self.assertTrue(error_norm < 1.e-14)
 
 
 if __name__=='__main__':
