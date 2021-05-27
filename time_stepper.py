9436210bbbe5050553fbb45c375a88dd90d9fbe3
Russell Hooper <rhoope@sandia.gov>
2021-05-27 11:46:31 -0600

    add forward difference demo

--- time_stepper.py
+++ time_stepper.py
@@ -60,16 +60,70 @@ def set_v():
     return
 
 
+def scenario_ForwardEuler(dt, nt):
+    '''
+    "Explicit" or "Forward" Euler time integration using example from
+         https://www.particleincell.com/2011/velocity-integration/
+
+    Input variables:
+        dt = time step size
+        nt = number of time steps
+    '''
+
+    # Model params
+    omega = 2*np.pi/(50.0*dt)
+
+    # Initial conditions
+    x0 = 1.0
+    v0 = 0.0
+
+    # Initialize time
+    time = 0.0
+
+    # This is the exact expression for the forcing term (acceleration)
+    accel = lambda x : -omega*omega*x
+
+    # Compute the exact solutions as well for later comparison
+    xexact = np.empty(nt+1)
+    vexact = np.empty(nt+1)
+
+    xsol = np.empty(nt+1)
+    vsol = np.empty(nt+1)
+
+    # Assign initial conditions
+    xsol[0] = x0
+    vsol[0] = v0
+    xexact[0] = x0
+    vexact[0] = v0
+
+    # Do time integration
+    for step in range(nt):
+        acc = accel(xsol[step])
+        vsol[step+1] = vsol[step] + dt*acc
+        xsol[step+1] = xsol[step] + dt*vsol[step]
+
+        # Compute exact values
+        time += dt
+        xexact[step+1] =  np.cos(omega*time)
+        vexact[step+1] = -np.sin(omega*time)
+
+    return xsol, vsol, xexact, vexact
+
+
+
 
 if __name__ == "__main__":
 
     # Start by trying to call the various functions
     #   ... use some nominal time stepping values ...
-    dt = 0.01
-    nt = 10
+    dt = 0.1
+    nt = 25
     v0 = 0.0 
     x0 = 1.0
 
-    time_step(dt, nt, v0, x0) 
+    #time_step(dt, nt, v0, x0) 
+    x, v, xe, ve = scenario_ForwardEuler(dt, nt)
+    for i in range(nt+1):
+        print(i, v[i], x[i], ve[i], xe[i])
 
     assert(True),'expected to make it this far wthout error.'
