6dc1949a203c39287b5c60c25f8451681e3f618d
Russell Hooper <rhoope@sandia.gov>
2021-06-18 08:29:00 -0600

    Add missing file

---
+++ particle.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Particle:
+
+    def __init__(self, ptype, specs):
+        self.type = ptype
+        self.charge = specs['charge']
+        self.mass = specs['mass']
+        self.weight = 1.0
+        self.pos = np.empty(0)
+        self.vel = np.empty(0)
+        self.field = np.empty(0)
