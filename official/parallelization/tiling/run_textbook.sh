ed30976f58fac979148a91579bfc43d2de6fdd70
J. Adam Stephens <jasteph@sandia.gov>
2024-06-21 12:14:10 -0600

    Switch dakota interface to system
    Flux seems to delay results file writing

--- official/parallelization/tiling/run_textbook.sh
+++ official/parallelization/tiling/run_textbook.sh
@@ -5,5 +5,6 @@ results=$2
 
 eval_id=$(grep eval_id $params | awk '{print $1}')
 hostname > host-${eval_id}.txt
-flux run -n 14 --label-io --output=eval-${eval_id}.out --error=eval-${eval_id}.err ./text_book_par $params $results 
+flux run -n 14 --label-io --output=eval-${eval_id}.out --error=eval-${eval_id}.err ./text_book_par $params temp-$results 
+mv temp-$results $results
 
