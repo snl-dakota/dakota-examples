68d799ff9a4cbc8f8e4e4a3a6c842f6fc900795b
J. Adam Stephens <jasteph@sandia.gov>
2024-06-21 12:17:30 -0600

    Fix prompt after batch driver exits
    Something in the batch driver or flux is leaving the terminal
    in a bad state after it exits. Output from Dakota is messed
    up, commands aren't echoed, etc. Add a comment and 'reset'
    command to fix it.

--- official/parallelization/batch_tiling/run_dakota.sh
+++ official/parallelization/batch_tiling/run_dakota.sh
@@ -10,3 +10,8 @@ export PSM2_DEVICES="self,shm,hfi"
 
 dakota -input dakota_sampling.in 2>&1 | tee dakota_sampling.log
 
+## On some systems, the study leaves the terminal in a bad state after
+## exiting (commands aren't echoed, prompt is preceded by lots of space, etc).
+## Uncomment the reset command to fix it.
+
+# reset
